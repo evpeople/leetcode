5585ba7d86189954eae20662e326eaefdad2ac32
evpeople <hangcaihui@gmail.com>
2022-05-24 21:47:26 +0800

    7

--- go/shoope/internal/f24/524.go
+++ go/shoope/internal/f24/524.go
@@ -188,3 +188,31 @@ func getKthFromEnd(head *ListNode, k int) *ListNode {
 	}
 	return head
 }
+func longestPalindrome(s string) string {
+	n := len(s)
+	dp := make([][]bool, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]bool, n)
+		dp[i][i] = true
+		if s[i] == s[i+1] {
+			dp[i][i+1] = true
+		}
+	}
+	begien := 0
+	maxLen := 1
+	for l := 2; l < n; l++ {
+		for i := 0; i < n; i++ {
+			j := l + i - 1
+			if j > n {
+				break
+			}
+			dp[i][j] = dp[i+1][j-1] && s[j] == s[i]
+			if dp[i][j] && j-i+1 > maxLen {
+				maxLen = j - i + 1
+				begien = i
+			}
+		}
+
+	}
+	return s[begien : begien+maxLen]
+}
