ddf2ecf02d2fbe27200d556e950f11b7a42d8705
evpeople <hangcaihui@gmail.com>
2022-05-24 21:50:44 +0800

    8

--- go/shoope/internal/f24/524.go
+++ go/shoope/internal/f24/524.go
@@ -3,6 +3,7 @@ package f24
 import (
 	"math"
 	"sort"
+	"strconv"
 )
 
 func threeSum(nums []int) [][]int {
@@ -215,4 +216,25 @@ func longestPalindrome(s string) string {
 
 	}
 	return s[begien : begien+maxLen]
+}
+func largestNumber(nums []int) string {
+	sort.Slice(nums, func(i, j int) bool {
+		x, y := nums[i], nums[j]
+		sx, sy := 10, 10
+		for sx <= x {
+			sx *= 10
+		}
+		for sy <= y {
+			sy *= 10
+		}
+		return sy*x+y > sx*y+x
+	})
+	if nums[0] == 0 {
+		return "0"
+	}
+	ans := []byte{}
+	for _, x := range nums {
+		ans = append(ans, strconv.Itoa(x)...)
+	}
+	return string(ans)
 }
