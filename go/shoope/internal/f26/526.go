5f42144a482744076142971c3533a892a61fced4
evpeople <hangcaihui@gmail.com>
2022-05-26 15:38:29 +0800

    1

--- go/shoope/internal/f26/526.go
+++ go/shoope/internal/f26/526.go
@@ -1,1 +1,19 @@
 package f26
+
+func generateParenthesis(n int) []string {
+	res := []string{}
+	var dfs func(Lremain int, rRemain int, path string)
+	dfs = func(Lremain, rRemain int, path string) {
+		if 2*n == len(path) {
+			res = append(res, path)
+		}
+		if Lremain > 0 {
+			dfs(Lremain-1, rRemain, path+"(")
+		}
+		if Lremain < rRemain {
+			dfs(Lremain, rRemain-1, path+")")
+		}
+	}
+	dfs(n, n, "")
+	return res
+}
