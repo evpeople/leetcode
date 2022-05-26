07271a6a0f8bf9b0dfb87d6adc7adf86d3ae6d6d
evpeople <hangcaihui@gmail.com>
2022-05-26 16:49:38 +0800

    4

--- go/shoope/internal/f26/526.go
+++ go/shoope/internal/f26/526.go
@@ -1,6 +1,9 @@
 package f26
 
-import "container/heap"
+import (
+	"container/heap"
+	"sort"
+)
 
 func generateParenthesis(n int) []string {
 	res := []string{}
@@ -101,12 +104,70 @@ func mergeKLists(lists []*ListNode) *ListNode {
 }
 
 func climbStairs(n int) int {
-	// dp := []int{0, 1, 2}
-	dp := make([]int, 0, 3)
+	// dp := make([]int, 0, 3)
 
-	dp = []int{0, 1, 2}
+	dp := [3]int{0, 1, 2}
 	for i := 3; i < n+1; i++ {
 		dp[i%3] = dp[(i+1)%3] + dp[(i+2)%3]
 	}
 	return dp[n%3]
+}
+
+var a []int
+
+type hp struct{ sort.IntSlice }
+
+func (h hp) Less(i, j int) bool  { return a[h.IntSlice[i]] > a[h.IntSlice[j]] }
+func (h *hp) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *hp) Pop() interface{} {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return v
+}
+
+func maxSlidingWindow2(nums []int, k int) []int {
+	a = nums
+	q := &hp{make([]int, k)}
+	for i := 0; i < k; i++ {
+		q.IntSlice[i] = i
+	}
+	heap.Init(q)
+
+	n := len(nums)
+	ans := make([]int, 1, n-k+1)
+	ans[0] = nums[q.IntSlice[0]]
+	for i := k; i < n; i++ {
+		heap.Push(q, i)
+		for q.IntSlice[0] <= i-k {
+			heap.Pop(q)
+		}
+		ans = append(ans, nums[q.IntSlice[0]])
+	}
+	return ans
+}
+func maxSlidingWindow(nums []int, k int) []int {
+	q := []int{}
+	push := func(i int) {
+		for len(q) > 0 && nums[i] >= nums[q[len(q)-1]] {
+			q = q[:len(q)-1]
+		}
+		q = append(q, i)
+	}
+
+	for i := 0; i < k; i++ {
+		push(i)
+	}
+
+	n := len(nums)
+	ans := make([]int, 1, n-k+1)
+	ans[0] = nums[q[0]]
+	for i := k; i < n; i++ {
+		push(i)
+		for q[0] <= i-k {
+			q = q[1:]
+		}
+		ans = append(ans, nums[q[0]])
+	}
+	return ans
 }
