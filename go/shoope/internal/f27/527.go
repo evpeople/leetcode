508bf91a98dd4408f0913fc54bf08156c255486f
evpeople <hangcaihui@gmail.com>
2022-05-27 21:29:37 +0800

    2

--- go/shoope/internal/f27/527.go
+++ go/shoope/internal/f27/527.go
@@ -48,6 +48,28 @@ func findRepeatNumber(nums []int) int {
 	}
 	return 0
 }
-func finde(wdsa int)int{
-	return 1
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil {
+		slow = slow.Next
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		if fast == slow {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
 }
