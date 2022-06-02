8a2081ee78ca2a6993279db3fa6876a1f1eba15e
evpeople <hangcaihui@gmail.com>
2022-06-02 21:42:15 +0800

    1

--- go/shoope/internal/j2/j2.go
+++ go/shoope/internal/j2/j2.go
@@ -1,1 +1,16 @@
 package j2
+
+func removeDuplicates(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+	slow := 1
+	for i := 1; i < l; i++ {
+		if nums[i] != nums[i-1] {
+			nums[slow] = nums[i]
+			slow++
+		}
+	}
+	return slow
+}
