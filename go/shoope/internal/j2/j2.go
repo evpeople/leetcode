9aff86d2311e24c57da8ec6bb691a56e82869531
evpeople <hangcaihui@gmail.com>
2022-06-02 21:52:34 +0800

    2

--- go/shoope/internal/j2/j2.go
+++ go/shoope/internal/j2/j2.go
@@ -14,3 +14,20 @@ func removeDuplicates(nums []int) int {
 	}
 	return slow
 }
+func minCostClimbingStairs(cost []int) int {
+	dp := make([]int, len(cost))
+	dp[0], dp[1] = cost[0], cost[1]
+	if len(cost) == 2 {
+		return min(dp[0], dp[1])
+	}
+	for i := 2; i < len(cost); i++ {
+		dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+	}
+	return min(dp[len(dp)-1], dp[len(dp)-2])
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
--- go/shoope/internal/j2/j2_test.go
+++ go/shoope/internal/j2/j2_test.go
@@ -1,6 +1,8 @@
 package j2
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_removeDuplicates(t *testing.T) {
 	type args struct {
@@ -17,6 +19,28 @@ func Test_removeDuplicates(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := removeDuplicates(tt.args.nums); got != tt.want {
 				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_minCostClimbingStairs(t *testing.T) {
+	type args struct {
+		cost []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test", args{[]int{10, 15, 20}}, 15},
+
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minCostClimbingStairs(tt.args.cost); got != tt.want {
+				t.Errorf("minCostClimbingStairs() = %v, want %v", got, tt.want)
 			}
 		})
 	}
